76f2e2c7ec457fbf261849449d4dd8460dd79cbd
18wh1a1253 <69300031+18wh1a1253@users.noreply.github.com>
2021-12-22 12:40:06 +0530

    Update program7.java

--- IS Lab/Program7/program7.java
+++ IS Lab/Program7/program7.java
@@ -2,26 +2,19 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.swing.JOptionPane;
+
 public class BlowFishCipher {
-public static void main(String[] args) throws Exception {
-// create a key generator based upon the Blowfish cipher
-KeyGeneratorkeygenerator = KeyGenerator.getInstance("Blowfish");
-// create a key
-// create a cipher based upon Blowfish Cipher
-cipher = Cipher.getInstance("Blowfish");
-// initialise cipher to with secret key
-cipher.init(Cipher.ENCRYPT_MODE, secretkey);
-// get the text to encrypt
-String inputText = JOptionPane.showInputDialog("Input your message:
-"); // encrypt message
-byte[] encrypted = cipher.doFinal(inputText.getBytes());
-// re-initialise the cipher to be in decrypt mode
-cipher.init(Cipher.DECRYPT_MODE, secretkey);
-// decrypt message
-byte[] decrypted = cipher.doFinal(encrypted);
-// and display the results
-JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),
-"\nEncrypted text: " + new String(encrypted) + "\n" +
-"\nDecrypted text: " + new String(decrypted));
-System.exit(0);
-} }
+    public static void main(String[] args) throws Exception {
+        KeyGenerator keygenerator = KeyGenerator.getInstance("Blowfish");
+        SecretKey secretkey = keygenerator.generateKey();
+        Cipher cipher = Cipher.getInstance("Blowfish");
+        cipher.init(Cipher.ENCRYPT_MODE, secretkey);
+        String inputText = JOptionPane.showInputDialog("Input your message: "); 
+        byte[] encrypted = cipher.doFinal(inputText.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, secretkey);
+        byte[] decrypted = cipher.doFinal(encrypted);
+       System.out.println ("Original String : " +inputText);
+       System.out.println ("\nEncrypted text: " + new String(encrypted));
+       System.out.println ("\nDecrypted text: " + new String(decrypted));
+    } 
+}
